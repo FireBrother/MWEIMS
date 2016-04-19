dc0c8efb2e1ea114b2085dd478bfe48aae4ebd68
Xian Wu <wuxian94@pku.edu.cn>
2016-04-19 13:05:02 +0800

    Added files via upload

---
+++ MWEIMS/result/deal.py
@@ -0,0 +1,34 @@
+# coding=utf8
+
+import codecs
+
+f1 = codecs.open('pmi_exact.txt', 'r', 'gbk')
+f2 = codecs.open('pmi_low.txt', 'r', 'gbk')
+f3 = codecs.open('pmi_high.txt', 'r', 'gbk')
+
+pmi_exact = {}
+pmi_low = {}
+pmi_high = {}
+
+pmi_ave = {}
+
+pair = [(f1, pmi_exact), (f2, pmi_low), (f3, pmi_high)]
+
+for p in pair:
+    for i, line in enumerate(p[0].readlines()):
+        line = line.strip()
+        w, v = line.split()
+        p[1][w] = float(v)
+
+of1 = codecs.open('exact_unique.txt', 'w', 'utf8')
+of2 = codecs.open('high_unique.txt', 'w', 'utf8')
+
+for w in sorted(pmi_exact.iteritems(), key = lambda x: -x[1]):
+    if w[0] not in pmi_high.keys():
+        print >>of1, w[0], w[1]
+
+for w in sorted(pmi_high.iteritems(), key = lambda x: -x[1]):
+    if w[0] not in pmi_exact.keys():
+        print >>of2, w[0], w[1]
+
+def diff(d1, d2,)
