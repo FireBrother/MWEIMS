d39eccf3bd0b17ed9c5147692835977797f3f9d1
Xian Wu <wuxian94@pku.edu.cn>
2016-04-26 01:20:32 +0800

    添加了评测文件

--- MWEIMS/experiment/evaluate.py
+++ MWEIMS/experiment/evaluate.py
@@ -12,13 +12,60 @@ for pt in pmi_types:
     for i, line in enumerate(files[pt].readlines()):
         line = line.strip()
         w, v = line.split()
+        w = ''.join(w.split('→'.decode('utf8')))
         pmis[pt][w] = float(v)
 
-for line in codecs.open('dict.txt', 'r', 'gbk')
+for line in codecs.open('dict.txt', 'r', 'gbk'):
     line = line.strip()
-        w, v = line.split()
-        dic.add(w)
+    w, v = line.split()
+    dic.add(w)
 
-for w in sorted(pmi_high.iteritems(), key = lambda x: -x[1]):
-    if w[0] not in pmi_new.keys():
-        print >>of2, w[0], w[1]
+N = len(dic)
+correct = {}
+error = {}
+precisoin = {}
+recall = {}
+fmeasure = {}
+
+topKs = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000]
+
+for pt in pmi_types:
+    correct[pt] = {}
+    error[pt] = {}
+    precisoin[pt] = {}
+    recall[pt] = {}
+    fmeasure[pt] = {}
+    for i, pair in enumerate(sorted(pmis[pt].iteritems(), key=lambda x: -x[1])):
+        w, _ = pair
+        if w in dic:
+            for topK in topKs:
+                if i < topK:
+                    v = correct[pt].get(topK, 0)
+                    correct[pt][topK] = v + 1
+        else:
+            for topK in topKs:
+                if i < topK:
+                    v = error[pt].get(topK, 0)
+                    error[pt][topK] = v + 1
+
+for pt in pmi_types:
+    for topK in topKs:
+        precisoin[pt][topK] = 1.0 * correct[pt][topK] / (correct[pt][topK] + error[pt][topK])
+        recall[pt][topK] = 1.0 * correct[pt][topK] / N
+        fmeasure[pt][topK] = 2.0 * precisoin[pt][topK] * recall[pt][topK] / (precisoin[pt][topK] + recall[pt][topK])
+
+of = codecs.open('evaluation.csv', 'w', 'utf8')
+output_pairs = [('precisoin', precisoin), ('recall', recall), ('f-measure', fmeasure)]
+for op in output_pairs:
+    of.write('{},\n'.format(op[0]))
+    of.write('pmi_type,')
+    for topK in topKs:
+        of.write('topK={},'.format(topK))
+    of.write('\n')
+    for pt in pmi_types:
+        of.write('{},'.format(pt))
+        for topK in topKs:
+            of.write('{:.2f},'.format(op[1][pt][topK]))
+        of.write('\n')
+    of.write('\n')
+
